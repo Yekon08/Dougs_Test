200ad0036267b7906aed0ebbc07ab58ade21ecae
MaximeJeannette <102291390+MaximeJeannette@users.noreply.github.com>
2023-03-11 01:18:28 +0100

    feat: setup & header unit test

---
+++ src/Components/__tests__/header.test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import * as React from "react";
+import "@testing-library/react/dont-cleanup-after-each";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import Header from "../Header";
+
+render(<Header activeFilter="group" />);
+
+describe("Header", () => {
+  test("Title should be Catégories", () => {
+    expect(screen.getByRole("heading")).toHaveTextContent("Catégories");
+  });
+
+  test("Selected btn should be Groupe de catégorie", () => {
+    expect(
+      screen.getByRole("button", {
+        name: /groupe de catégorie/i,
+      })
+    ).toHaveClass("activeBtn");
+  });
+});
